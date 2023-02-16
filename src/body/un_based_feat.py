a1fc0fbf93fffa2dbe4f3acb1a59406f20615045
RealXun <scrunxun@gmail.com>
2023-02-16 20:32:06 +0100

    modified:   src/body/un_based_feat.py

--- src/body/un_based_feat.py
+++ src/body/un_based_feat.py
@@ -62,9 +62,9 @@ def uns_feat():
 
         # Create the multiselect widgets
         selected_genre = st.multiselect('Select genre', option_genre)
-        selected_type = st.multiselect('Select type', option_type)
+        selected_type = st.multiselect('Select type', option_type, max_selections=1)
     else:
-        st.text("AND method would match any gender you input with the type. More Genres, more results")
+        st.text("AND method would match any gender you input with the type.\n More Genres, more results \n Type should be one, there is no anime with two types at ones")
         # Define the options for the multiselects
         option_genre = ['Drama', 'Romance', 'School', 'Supernatural', 'Action',
         'Adventure', 'Fantasy', 'Magic', 'Military', 'Shounen', 'Comedy',
@@ -78,7 +78,7 @@ def uns_feat():
 
         # Create the multiselect widgets
         selected_genre = st.multiselect('Select genre', option_genre)
-        selected_type = st.multiselect('Select type', option_type)
+        selected_type = st.multiselect('Select type', option_type, max_selections=1)
 
     criteria_selected = to_search and user_input and selected_genre and selected_type
 
