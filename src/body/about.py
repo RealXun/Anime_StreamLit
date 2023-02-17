a8e59e6916d051dddb6745ad278ddbaf0222efde
RealXun <scrunxun@gmail.com>
2023-02-17 16:48:10 +0100

    markdown

--- src/body/about.py
+++ src/body/about.py
@@ -37,16 +37,5 @@ def it_is_about():
     st.markdown('<p class="font">About the work</p>', unsafe_allow_html=True)
     st.write("The goal of this project is that according to the user's anime viewing history we can recommend a list of anime that suits their tastes.\nIn order to do this we are going to create 3 types or recommendation system")
 
-    with open(body_folder + "/" + "about_text.md",'r') as f:
-        readme_line = f.readlines()
-        readme_buffer = []
-        # resource_files
-    for line in readme_line :
-        readme_buffer.append(line) 
-        for image in images:
-            if image in line:
-                st.markdown(''.join(readme_buffer[:-1])) 
-                st.image(f'images/{image}')
-                readme_buffer.clear()
-                
-    st.markdown(''.join(readme_buffer))
+    with open(body_folder + "about_text.md",'r') as f:
+        st.markdown(f.read(), unsafe_allow_html=True)
