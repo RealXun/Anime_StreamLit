cddc8567408c78097144ee93c713b0518753beb7
RealXun <scrunxun@gmail.com>
2023-02-17 16:34:08 +0100

    import

--- src/body/about.py
+++ src/body/about.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from PIL import Image
 import requests
 from io import BytesIO
+import glob
 
 PROJECT_ROOT = os.path.abspath(os.path.join(
                   os.path.dirname(__file__), 
@@ -21,9 +22,10 @@ saved_models_folder = (data_folder + "/" + "saved_models")
 raw_data = (data_folder + "/" + "_raw")
 processed_data = (data_folder + "/" + "processed")
 content_based_supervised_data = (data_folder + "/" + "processed" + "/" + "content_based_supervised")
+images = (PROJECT_ROOT + "/" + "images")
+cover_images = (images + "/" + "Cover_images")
 
-def read_markdown_file(markdown_file):
-    return Path(markdown_file).read_text()
+
 
 def it_is_about():
     #Add the cover image for the cover page. Used a little trick to center the image
@@ -34,8 +36,18 @@ def it_is_about():
         </style> """, unsafe_allow_html=True)
     st.markdown('<p class="font">About the work</p>', unsafe_allow_html=True)
     st.write("The goal of this project is that according to the user's anime viewing history we can recommend a list of anime that suits their tastes.\nIn order to do this we are going to create 3 types or recommendation system")
-    intro_markdown = read_markdown_file("about_text.md")
-    st.markdown(intro_markdown, unsafe_allow_html=True)
 
     with open(body_folder + "about_text.md",'r') as f:
-        st.markdown(f.read(), unsafe_allow_html=True)
+        readme_line = f.readlines()
+        readme_buffer = []
+        resource_files = [os.path.basename(x) for x in glob.glob(f'images/*')]
+        # resource_files
+    for line in readme_line :
+        readme_buffer.append(line) 
+        for image in resource_files:
+            if image in line:
+                st.markdown(''.join(readme_buffer[:-1])) 
+                st.image(f'Resources/{image}')
+                readme_buffer.clear()
+                
+    st.markdown(''.join(readme_buffer))
