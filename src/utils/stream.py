39bdc94e3480ec9232d6cdf14ffd227e02469776
RealXun <scrunxun@gmail.com>
2023-02-18 13:22:41 +0100

    up

--- src/utils/stream.py
+++ src/utils/stream.py
@@ -1,23 +1,20 @@
-# data analysis and wrangling
-import pandas as pd
-import numpy as np
-import warnings
+import streamlit as st
 import os
-import re
 import sys
-import warnings
-import joblib
+from utils import recommend
+from PIL import Image
 import pickle
-from fastparquet import write
-from fuzzywuzzy import fuzz
+import requests
 from pathlib import Path
-import zipfile
-import shutil
+from streamlit_option_menu import option_menu
+from PIL import Image
+import requests
+from io import BytesIO
 from body import testing
 from body import un_based_rate
 from body import un_based_feat
 from body import sup_id
 from body import about
 from body import about_me
-from utils import recommend
+
 
