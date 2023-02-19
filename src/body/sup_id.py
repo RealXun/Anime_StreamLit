4b1dc4d456e3c2bc9d2b475698bec1d4159b9fff
RealXun <scrunxun@gmail.com>
2023-02-19 17:11:21 +0100

    update

--- src/body/sup_id.py
+++ src/body/sup_id.py
@@ -22,36 +22,61 @@ def user_id():
     st.title('Supervised user rating based collaborative filtering')
 
 
-     # Get the user's favorite movie
-    user_input_1  = st.text_input("Choose the ID of the user you would like to see recommendations?")
+# Getting user input in the form of an integer for the ID of a user for whom 
+# the anime recommendation will be generated. The code first asks the user 
+# to input the ID, then it tries to convert the user input to an integer. 
+# If it's successful, it returns a success message with the integer value. 
+# If it's not successful, it returns an error message asking the user to 
+# enter a valid integer.
+
+    user_input_1  = st.text_input("Choose the ID of the user you would like to see recommendations?") # create a text input for the user to enter the ID of the user they want recommendations for
     try:
-        users_id = int(user_input_1)
+        users_id = int(user_input_1) # convert the input to an integer
     except ValueError:
-        st.error("Please enter a valid integer.")
+        st.error("Please enter a valid integer.") # show an error message if the input is not a valid integer
 
-    if isinstance(user_input_1, int):
-        st.success(f"You entered the integer: {user_input_1}")
+    if isinstance(user_input_1, int): # check if the input is an integer
+        st.success(f"You entered the integer: {user_input_1}") # show a success message with the input value
 
-    # Get the number of the recomemendations the users wants
-    user_input  = st.text_input("What is the maximum number of recommendations you would like to get?:")
+
+
+# This code prompts the user to enter the maximum number of recommendations 
+# they would like to receive, then it attempts to convert the user input into 
+# an integer using a try-except block. If the user input can be converted to 
+# an integer, it is stored in the variable number_of_recommendations. If the 
+# conversion fails, an error message is displayed. If the user input is an 
+# integer, a success message is displayed.
+
+    # Prompts the user to enter a maximum number of recommendations 
+    # they want to get and stores the input in the 'user_input' variable.
+    user_input  = st.text_input("What is the maximum number of recommendations you would like to get?:") 
+
+    # The input is then converted into an integer type and stored in the 
+    # 'number_of_recommendations' variable using the 'int' function. 
     try:
         number_of_recommendations = int(user_input)
     except ValueError:
         st.error("Please enter a valid integer.")
 
+    # If the input is successfully converted to an integer, a success message 
+    # is displayed with the integer value using the 'st.success' function.
     if isinstance(user_input, int):
         st.success(f"You entered the integer: {user_input}")
 
     
 
-    # Define your filtering method (and/or)
-    method = st.selectbox("Choose a filtering method", ["and", "or"])
-     # Create the multiselect widgets
+'''
+The code presents a dropdown menu to select between two filtering methods ("and" and "or"). 
+Depending on the method chosen, the user can select one or more genres and one or more types 
+of anime using checkboxes. If the "and" method is chosen, the user can only select one type 
+of anime, while the "or" method allows the user to select multiple types. The selected genres 
+and types are stored in the selected_genre and selected_type variables.
+'''
+    method = st.selectbox("Choose a filtering method", ["and", "or"]) # prompts user to choose filtering method either 'and' or 'or'
 
-    if method == "or":
+    if method == "or": # if filtering method is 'or'
 
-        # Define the options for the multiselects
-        option_genre = ['Drama', 'Romance', 'School', 'Supernatural', 'Action',
+        option_genre = ['Drama', 'Romance', 'School', 'Supernatural', 'Action', # list of anime genres
         'Adventure', 'Fantasy', 'Magic', 'Military', 'Shounen', 'Comedy',
         'Historical', 'Parody', 'Samurai', 'Sci-Fi', 'Thriller', 'Sports',
         'Super Power', 'Space', 'Slice of Life', 'Mecha', 'Music',
@@ -59,15 +84,15 @@ def user_id():
         'Police', 'Psychological', 'Demons', 'Ecchi', 'Josei',
         'Shounen Ai', 'Game', 'Dementia', 'Harem', 'Cars', 'Kids',
         'Shoujo Ai', 'Hentai', 'Yaoi', 'Yuri']
-        option_type = ['ALL','Movie', 'TV', 'OVA', 'Special', 'Music', 'ONA']
+        option_type = ['ALL','Movie', 'TV', 'OVA', 'Special', 'Music', 'ONA'] # list of anime types
 
-        # Create the multiselect widgets
-        selected_genre = st.multiselect('Select genre', option_genre)
-        selected_type = st.multiselect('Select type', option_type)
-    else:
+        selected_genre = st.multiselect('Select genre', option_genre) # prompts user to select genres
+        selected_type = st.multiselect('Select type', option_type) # prompts user to select anime types
+
+    else: # if filtering method is 'and'
         st.text("AND method would match any gender you input with the type.\n More Genres, more results \n Type should be one, there is no anime with two types at once")
-        # Define the options for the multiselects
-        option_genre = ['Drama', 'Romance', 'School', 'Supernatural', 'Action',
+
+        option_genre = ['Drama', 'Romance', 'School', 'Supernatural', 'Action', # list of anime genres
         'Adventure', 'Fantasy', 'Magic', 'Military', 'Shounen', 'Comedy',
         'Historical', 'Parody', 'Samurai', 'Sci-Fi', 'Thriller', 'Sports',
         'Super Power', 'Space', 'Slice of Life', 'Mecha', 'Music',
@@ -75,11 +100,11 @@ def user_id():
         'Police', 'Psychological', 'Demons', 'Ecchi', 'Josei',
         'Shounen Ai', 'Game', 'Dementia', 'Harem', 'Cars', 'Kids',
         'Shoujo Ai', 'Hentai', 'Yaoi', 'Yuri']
-        option_type = ['Movie', 'TV', 'OVA', 'Special', 'Music', 'ONA']
+        option_type = ['Movie', 'TV', 'OVA', 'Special', 'Music', 'ONA'] # list of anime types
 
-        # Create the multiselect widgets
-        selected_genre = st.multiselect('Select genre', option_genre)
-        selected_type = st.multiselect('Select type', option_type, max_selections=1)
+        selected_genre = st.multiselect('Select genre', option_genre) # prompts user to select genres
+        selected_type = st.multiselect('Select type', option_type, max_selections=1) # prompts user to select anime types, allowing only one selection
+
 
     criteria_selected = user_input_1 and user_input and selected_genre and selected_type
 
