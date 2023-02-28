1e5d487dfacbf3a88b9259e086616feb412a9bca
RealXun <scrunxun@gmail.com>
2023-02-28 02:09:46 +0100

    up

--- src/body/sup_id.py
+++ src/body/sup_id.py
@@ -18,6 +18,18 @@ import base64
 
 output = BytesIO()
 
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
 
 def user_id():
 #Add the cover image for the cover page. Used a little trick to center the image
@@ -146,6 +158,9 @@ def user_id():
 
     # Enable button if both criteria are selected
     if st.button('Get the Recommendation', disabled=not criteria_selected):
+        st.download_button(label='📥 Download Current Result',
+                                    data=df_xlsx ,
+                                    file_name= 'df_test.xlsx')
         with st.spinner('Generating recommendations...'):
             result = super_ratings_based(users_id,number_of_recommendations,selected_genre,selected_type, method)
             if result is not None: 
@@ -160,23 +175,9 @@ def user_id():
                         new_dict[di['name']][k]=di[k]
                 # Define the DataFrame
                 df = pd.DataFrame.from_dict(new_dict)
-                def to_excel(df):
-                    output = BytesIO()
-                    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-                    df.to_excel(writer, index=False, sheet_name='Sheet1')
-                    workbook = writer.book
-                    worksheet = writer.sheets['Sheet1']
-                    format1 = workbook.add_format({'num_format': '0.00'}) 
-                    worksheet.set_column('A:A', None, format1)  
-                    writer.save()
-                    processed_data = output.getvalue()
-                    return processed_data
+
                 df_xlsx = to_excel(df)
-                st.download_button(label='📥 Download Current Result',
-                                                data=df_xlsx ,
-                                                file_name= 'df_test.xlsx')
-
-
+    
 
                 # Determine how many rows and columns are needed to display the recommendations
                 num_cols = 5
